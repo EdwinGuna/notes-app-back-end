43e7cc8d4e056711591ed9c39e375a127a37cb8d
Medwin Gun <medwin4belitung@gmail.com>
2023-09-07 10:49:19 +0700

    Postgres implemented & port 9000

---
+++ src/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+//const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService')
+const NotesValidator = require('./validator/notes/index');
+
+const init = async () => {
+  const notesService = new NotesService();
+  const server = Hapi.server(
+  {
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+    
+  });
+
+  //server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+  
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+  //console.log('Server running on %s', server.info.uri);
+
+};
+
+init();
